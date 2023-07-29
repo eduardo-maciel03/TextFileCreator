adaae732a0d448446cd2df05247b7f4d1cd97311
Eduardo Maciel <eduardomacielpereira@hotmail.com>
2023-07-29 20:11:12 -0300

    Update GenericList.cs

--- TextFileCreator/GenericList.cs
+++ TextFileCreator/GenericList.cs
@@ -3,7 +3,7 @@ namespace TextFileCreator
 {
     public class GenericList<TYPE>
     {
-        //This class creates a new list every time you call it, and you can pass it any type tou like
+        //This class creates a new list every time you call it, and you can pass it any type you like
         public GenericList() 
         {
             MyList = new List<TYPE>();
